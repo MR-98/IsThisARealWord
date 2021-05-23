48e4d9287aabee05e62b29c7d3b43bcd5144a83a
Mateusz Rosiak <215866@edu.p.lodz.pl>
2021-05-23 13:04:52 +0200

    Remove default page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -9,23 +9,4 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'IsThisARealWord'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('IsThisARealWord');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('IsThisARealWord app is running!');
-  });
 });
